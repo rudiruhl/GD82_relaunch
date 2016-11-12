2a64dc5b315af0f66e7b8d7b564ee62fae60bc74
rudiruhl <rudigd82@gmail.com>
2016-11-12 15:48:05 +0100

    Adding Gulp Stuff: watchTask and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var browserSync = require('browser-sync').create();
 
 // Tasks
-
-gulp.task('sass', function(){
-  return gulp.src('assets/css/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+gulp.task('sass', function() {
+  return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('assets/css/**/*.scss', ['sass']);
+  // Other watchers
 });
