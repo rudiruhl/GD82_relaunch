4d92e9e86a3ed7aa2977f45b88e5ac5188ea3b44
rudiruhl <rudigd82@gmail.com>
2016-11-12 14:17:49 +0100

    Initial Commit

---
+++ assets/js/function.js
@@ -0,0 +1,11 @@
+$(function() {
+  mobileNav();
+});
+
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
