e934ac0f8f18196320ebce1afa79988176079ea8
rudiruhl <rudigd82@gmail.com>
2016-11-19 09:08:32 +0100

    :rocket: Add gallery.js

---
+++ assets/js/gallery.js
@@ -0,0 +1,27 @@
+  $(document).ready(function () {
+          $('.mygallery').justifiedGallery({
+                rowHeight: 120,
+                maxRowHeight: null,
+                margins: 1,
+                border: 0,
+                rel: 'liveDemo',
+                lastRow: 'justify',
+                captions: false,
+                randomize: false,
+                sizeRangeSuffixes: {
+                    lt100: '_t',
+                    lt240: '_m',
+                    lt320: '_n',
+                    lt500: '',
+                    lt640: '_z',
+                    lt1024: '_b'
+                }
+            }).on('jg.complete', function () {
+                $(this).find('a').colorbox({
+                    maxWidth: '80%',
+                    maxHeight: '80%',
+                    opacity: 0.8,
+                    transition: 'elastic',
+                    current: ''
+                });
+            });
