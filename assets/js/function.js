92a9ea6d79d30bcbee7a5eb585ab49524648c7ae
rudiruhl <rudigd82@gmail.com>
2016-11-19 08:21:46 +0100

    :art: improving the format/structure of the code

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,11 +1,14 @@
 $(function() {
-  mobileNav();
+    mobileNav();
 });
 
 function mobileNav() {
-  $('.mobile-nav-toggle').on('click', function(){
-    var status = $(this).hasClass('is-open');
-    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
-    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
-  });
+    $('.mobile-nav-toggle').on('click', function() {
+        var status = $(this).hasClass('is-open');
+        if (status) {
+            $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open');
+        } else {
+            $('.mobile-nav-toggle, .mobile-nav').addClass('is-open');
+        }
+    });
 }
