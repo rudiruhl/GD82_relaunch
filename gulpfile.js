fc3d33373da1b9fde96179a609778bd8e03e7193
Rudi Ruhl <rudiruhl@rudiruhl-loki-0.4>
2016-11-22 21:54:03 +0100

    :shirt: Remove linter warnings

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,24 @@ var browserSync = require('browser-sync').create();
 
 // Tasks
 gulp.task('sass', function() {
-  return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in app/scss
-    .pipe(sass())
-    .pipe(gulp.dest('assets/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src('assets/css/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    });
+});
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('assets/css/**/*.sass', ['sass']);
-  gulp.watch('assets/css/**/*.scss', ['sass']);
-  // Other watchers
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('assets/css/**/*.sass', ['sass']);
+    gulp.watch('assets/css/**/*.scss', ['sass']);
+    // Other watchers
 });
